2fc62f7402a2a3f7de2460afc31079ebb21b5e80
qduc159 <qduc159@gmail.com>
2017-07-06 00:00:06 +0700

    Check win condition

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
 import Board from './Board';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      winner: null
+    }
+    this.endGameHandler = this.endGameHandler.bind(this);
+  }
+
+  endGameHandler(winner) {
+    this.setState({
+        winner
+    });
+  }
+
   render() {
+    let message = this.state.winner === null ? null : (<div>Player {this.state.winner} win!</div>);
     return (
       <div className="App">
-        <Board size="8"/>
+          {message}
+          <div><Board size="8" handleEndGame={this.endGameHandler}/></div>
       </div>
     );
   }
--- src/Board.js
+++ src/Board.js
@@ -5,6 +5,7 @@ class Board extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            gameEnded: false,
             thisTurn: 0,
             board: []
         };
@@ -22,16 +23,99 @@ class Board extends Component {
     }
 
     clickHandler(row, col) {
+        if (this.state.gameEnded) {
+            return;
+        }
+
         if (this.state.board[row][col] === null) {
+            // Update board
             let newBoard = this.state.board.slice();
             newBoard[row][col] = this.state.thisTurn;
             this.setState({
-                thisTurn: 1 - this.state.thisTurn
-            });
-            this.setState({
                 board: newBoard
             });
+
+            // Did someone win?
+            let win = this.checkWin(this.props.size, row, col, this.state.board);
+            if (win) {
+                this.setState({gameEnded: true});
+                this.props.handleEndGame(this.state.thisTurn);
+                return;
+            }
+
+            // If not, change turn and continue
+            this.setState({
+                thisTurn: 1 - this.state.thisTurn
+            });
         }
+    }
+
+    checkWin(size, row, col, board) {
+        const currentPlayer = board[row][col];
+        let offset = 1;
+        let direction = {
+            up: 1,
+            down: 1,
+            left: 1,
+            right: 1,
+            ul: 1,
+            ur: 1,
+            dl: 1,
+            dr: 1,
+        };
+        while (offset < 5) {
+            if (col + offset === size) {
+                direction.right = 0;
+                direction.ur = 0;
+                direction.dr = 0;
+            }
+            if (row + offset === size) {
+                direction.down = 0;
+                direction.dl = 0;
+                direction.dr = 0;
+            }
+            if (col - offset < 0) {
+                direction.left = 0;
+                direction.ul = 0;
+                direction.dl = 0;
+            }
+            if (row - offset < 0) {
+                direction.up = 0;
+                direction.ul = 0;
+                direction.ur = 0;
+            }
+
+            let cell = {
+                up: direction.up === 0 ? null : board[row - offset][col],
+                down: direction.down === 0 ? null : board[row + offset][col],
+                left: direction.left === 0 ? null : board[row][col - offset],
+                right: direction.right === 0 ? null : board[row][col + offset],
+                ul: direction.ul === 0 ? null : board[row - offset][col - offset],
+                ur: direction.ur === 0 ? null : board[row - offset][col + offset],
+                dl: direction.dl === 0 ? null : board[row + offset][col - offset],
+                dr: direction.dr === 0 ? null : board[row + offset][col + offset],
+            };
+
+            for (let k in cell) {
+                if (direction[k] !== 0 && cell[k] === currentPlayer) {
+                    direction[k]++;
+                } else {
+                    direction[k] = 0;
+                }
+            }
+
+            if ((direction.up + direction.down === 5) ||
+                (direction.left + direction.right === 5) ||
+                (direction.ul + direction.dr === 5) ||
+                (direction.ur + direction.dl === 5))
+            {
+                return true;
+            }
+
+            offset++;
+        }
+
+        return false;
     }
 
     render() {
